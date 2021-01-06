d4634de0a11a6e1904c6609dc790533006fbc254
poychang <poypost@gmail.com>
2021-01-06 15:03:01 +0800

    update

--- build.ps1
+++ build.ps1
@@ -1,14 +1,14 @@
-function Get-Joke {
-    $targetUrl = "https://api.jokes.one/jod"
+function Get-Quote {
+    $targetUrl = "https://quotes.rest/qod?language=en"
     $Headers = @{ 'Content-Type' = 'application/json'; }
     $Response = Invoke-WebRequest $targetUrl -SessionVariable 'Session' -Method 'GET' -Headers $Headers
-    Write-Output ($Response.Content | ConvertFrom-JSON).contents.jokes[0].joke.text
+    Write-Output ($Response.Content | ConvertFrom-JSON).contents.quotes[0].quote
 }
 
 $readme = @"
 # Hello World $(Get-Date -Format 'yyyy/MM/dd')
 
-$(Get-Joke)
+$(Get-Quote)
 "@
 
 $readme | Out-File -FilePath .\README.md
